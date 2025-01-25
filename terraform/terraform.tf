d22dc45631388ff86391eaf4df0fde520662424f
minh <minh@gmail.com>
2025-01-25 16:04:02 +0700

    feat: first commit after fork repository

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,12 +27,12 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "gitopsterrastate"
+    bucket = "vprofileactions-minh-gitops-practice"
     key    = "terraform.tfstate"
     region = "us-east-2"
   }
 
-  required_version = "~> 1.6.3"
+  required_version = ">= 1.6.3, < 2.0.0"
 }
 ##
 ##
